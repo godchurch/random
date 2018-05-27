65b6857c4e2103c84c75765974bcfe727761eed2
godchurch <37278203+godchurch@users.noreply.github.com>
2018-05-27 04:05:27 -1000

    shell defaults

---
+++ shell_defaults.sh
@@ -0,0 +1,47 @@
+#####################################################################
+# BOURNE SHELL
+#####################################################################
+IFS='
+'
+OPTIND='1'
+PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+PPID='6262'
+PS1='$ '
+PS2='> '
+PS4='+ '
+PWD='/wibble/wobble/wubble'
+
+#####################################################################
+# BOURNE AGAIN SHELL
+#####################################################################
+BASH=/bin/bash
+BASHOPTS=cmdhist:complete_fullquote:extquote:force_fignore:hostcomplete:interactive_comments:progcomp:promptvars:sourcepath
+BASH_ALIASES=()
+BASH_ARGC=()
+BASH_ARGV=()
+BASH_CMDS=()
+BASH_EXECUTION_STRING=set
+BASH_LINENO=()
+BASH_SOURCE=()
+BASH_VERSINFO=([0]="4" [1]="4" [2]="12" [3]="1" [4]="release" [5]="x86_64-pc-linux-gnu")
+BASH_VERSION='4.4.12(1)-release'
+DIRSTACK=()
+EUID=1000
+GROUPS=()
+HOSTNAME=crucial
+HOSTTYPE=x86_64
+IFS=$' \t\n'
+MACHTYPE=x86_64-pc-linux-gnu
+OPTERR=1
+OPTIND=1
+OSTYPE=linux-gnu
+PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:.
+PPID=6262
+PS4='+ '
+PWD=/wibble/wobble/wubble
+SHELL=/bin/bash
+SHELLOPTS=braceexpand:hashall:interactive-comments
+SHLVL=1
+TERM=dumb
+UID=1000
+_=bash
